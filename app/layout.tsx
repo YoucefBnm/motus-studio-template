b3d35d7916055026e181ee427775fea1001a40d4
Josef Bnm <ssefbnm@gmail.com>
2026-04-04 19:33:23 +0100

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css";
 import { Header } from "@/sections/header";
 import { Footer } from "@/sections/footer";
 import { Analytics } from "@vercel/analytics/next";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Motus Studio",
@@ -15,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className="antialiased">
         <Header />
         {children}
